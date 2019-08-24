328950d195eec77043e8938b1ff8c0ba0b986d33
Kenny Le <kennyle37@gmail.com>
2019-08-24 16:49:45 -0700

    lc21 and lc242

--- exercises/LC252_canAttendMeetings/index.js
+++ exercises/LC252_canAttendMeetings/index.js
@@ -4,6 +4,26 @@
 // canAttendMeetings([[0, 30], [5, 10], [15, 20]]) --> false
 // canAttendMeetings([[7, 10], [2, 4]]) --> true
 
-const canAttendMeetings = intervals => {};
+const canAttendMeetings = function (intervals) {
+  if (intervals.length === 0) return true;
+  
+  intervals.sort((a, b) => {
+      return a[0] - b[0];
+  })
+  let end = intervals[0][1];
+  
+  for (let i = 1; i < intervals.length; i++) {
+      const currentMeeting = intervals[i];
+      const currentMeetingStart = currentMeeting[0];
+      const currentMeetingEnd = currentMeeting[1];
+      
+      if (currentMeetingStart <= end) {
+          return false;
+      } else {
+          end = currentMeetingEnd;
+      }
+  }
+  return true;
+}
 
 module.exports = canAttendMeetings;
